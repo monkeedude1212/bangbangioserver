f892dccd9178f499a6fcb109c40c3c86c27b6b31
root <root@DESKTOP-DB8L142.localdomain>
2025-02-06 22:44:18 -0700

    versioning

--- serverMain.js
+++ serverMain.js
@@ -3,12 +3,13 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     
     var date = new Date();
     const data = {
         message: 'Hello World from the server!',
         time: date.toISOString(),
+        version: 1
     }
     res.writeHead(200, {'content-type': 'application/json'});
     res.end(JSON.stringify(data));
