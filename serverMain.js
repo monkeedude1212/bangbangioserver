64ada823fa7909c779351e272397122f705e7b38
root <root@DESKTOP-DB8L142.localdomain>
2025-02-06 22:27:56 -0700

    include server time

--- serverMain.js
+++ serverMain.js
@@ -1,8 +1,11 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+    
+    var date = new Date();
     const data = {
-        message: 'Hello World from the server!'
+        message: 'Hello World from the server!',
+        time: date.toISOString(),
     }
     res.writeHead(200, {'content-type': 'application/json'});
     res.end(JSON.stringify(data));
